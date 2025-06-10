e1d861e52a9273d95e0ebb8abe829645318fe928
Kancharana Jyotsna <164002267+Jyotsna2409@users.noreply.github.com>
2025-06-10 15:04:43 +0530

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,6 @@
 from pyngrok import ngrok
 
-# Run the app
 !streamlit run app.py &
 
-# Expose via ngrok
 public_url = ngrok.connect(port=8501)
 print(f"🌐 Your Streamlit app is live at: {public_url}")
